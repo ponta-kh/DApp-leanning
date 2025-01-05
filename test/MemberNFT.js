b8a8304d2ee5548112053bce924e33f8efe1a8ee
ponta-hometownPC <example@gmail.com>
2025-01-05 23:33:24 +0900

    make template

---
+++ test/MemberNFT.js
@@ -0,0 +1,10 @@
+const {expect} = require("chai");
+
+describe("", function(){
+    it("トークン名称・シンボルがセットされている", async function(){
+        expect().to.equal(0);
+    });
+    it("デプロイアドレスがownerと一致", async function(){
+        expect().to.equal(0);
+    });
+})
